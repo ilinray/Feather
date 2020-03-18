813a7eed7f88be15bc70e50544e7d744e1279cee
Poweedlou <balalin2003@gmail.com>
2020-03-18 18:22:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Api
 import json
 
@@ -10,6 +10,11 @@ app = Flask(__name__, template_folder='frontend', static_url_path="/")
 @app.route('/')
 def main():
     return render_template("index.html")
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        request.form
 
 
 if __name__ == '__main__':
