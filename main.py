a24f64abad01e8670a188b00dc58064962163829
SuhEugene <suheugene@yandex.ru>
2020-03-18 18:18:15 +0300

    login page

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ app = Flask(__name__, template_folder='frontend', static_url_path="/")
 def main():
     return render_template("index.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0', debug=True)
