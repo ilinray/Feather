016b8b32f36b78e60473af0dcf81d12a42dd4a0f
Poweedlou <balalin2003@gmail.com>
2020-03-18 18:40:02 +0300

    s

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Api
 import json
 
 
 app = Flask(__name__, template_folder='frontend', static_url_path="/")
-app.secret_key = 'SOME_KEY_qwertyuiop'
 # api = Api(app)
 
 
@@ -12,9 +11,13 @@ app.secret_key = 'SOME_KEY_qwertyuiop'
 def main():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        request.form['']
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0', debug=True)
