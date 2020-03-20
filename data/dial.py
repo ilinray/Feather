f7088b5760c26320721de6d359b4ff5423b6ec30
Poweedlou <balalin2003@gmail.com>
2020-03-20 19:40:06 +0300

    Update dial.py

--- data/dial.py
+++ data/dial.py
@@ -4,10 +4,11 @@ import sys
 from .db_funcs import UserConnector, DialogConnector
 
 
-def dialogs_resources(app):
+def chats_resource(app):
     class ChatsResource(Resource):
         def get(self):
             parser = reqparse.RequestParser()
             parser.add_argument('uid', required=True)
             args = parser.parse_args()
-            
+            if args['uid'] != app.session['logged_in']:
+                return jsonify({})
