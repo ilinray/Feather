f54be47e68cca57f3c25eaf4e9387723de6400d6
Poweedlou <balalin2003@gmail.com>
2020-03-20 19:43:57 +0300

    fix

--- data/dial.py
+++ data/dial.py
@@ -1,14 +1,13 @@
 from flask_restful import reqparse, Resource
-from flask import jsonify
+from flask import jsonify, session
 import sys
 from .db_funcs import UserConnector, DialogConnector
 
 
-def chats_resource(app):
-    class ChatsResource(Resource):
-        def get(self):
-            parser = reqparse.RequestParser()
-            parser.add_argument('uid', required=True)
-            args = parser.parse_args()
-            if args['uid'] != app.session['logged_in']:
-                return jsonify({})
+class ChatsResource(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('uid', required=True)
+        args = parser.parse_args()
+        if args['uid'] != session['logged_in']:
+            return jsonify({})
