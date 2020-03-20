1606b11a3a36194b25f8f27388a5be297c7d144d
Poweedlou <balalin2003@gmail.com>
2020-03-20 18:47:49 +0300

    login security

--- data/auth.py
+++ data/auth.py
@@ -1,24 +1,35 @@
 from flask_restful import reqparse, abort, Resource
-from flask import jsonify
+from flask import jsonify, abort
 import sys
 from .db_funcs import UserConnector
 
-class AuthResource(Resource):
 
-    # Log in
-    def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('login', required=True)
-        parser.add_argument('password', required=True)
-        args = parser.parse_args()
-        return jsonify({'status': "ER"})
+def auth_resource(app):
+    class AuthResource(Resource):
+        # Log in
+        def get(self):
+            parser = reqparse.RequestParser()
+            parser.add_argument('login', required=True)
+            parser.add_argument('password', required=True)
+            args = parser.parse_args()
+            try:
+                user = UserConnector.login(args['login'], args['password'])
+                if user is not None:
+                    app.session['logged_in'] = user.id
+                    return jsonify({'status': "OK",
+                                    'uid': user.id})
+                else:
+                    return jsonify({'status': "ER"}), 401
+            except:
+                return jsonify({'status': "ER"}), 404
 
-    # Sign up
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('login', required=True)
-        parser.add_argument('email', required=True)
-        parser.add_argument('password', required=True)
-        args = parser.parse_args()
-        uid = UserConnector.new_user(args['login'], args['email'], args['password']).id
-        return jsonify({'status': "OK", 'uid': uid})
+        # Sign up
+        def post(self):
+            parser = reqparse.RequestParser()
+            parser.add_argument('login', required=True)
+            parser.add_argument('email', required=True)
+            parser.add_argument('password', required=True)
+            args = parser.parse_args()
+            uid = UserConnector.new_user(args['login'], args['email'], args['password']).id
+            app.session['logged_in'] = uid
+            return jsonify({'status': "OK", 'uid': uid})
