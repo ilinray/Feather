6787e5f99bfaa417cf92fbf93c7a902d0e6c93c6
Poweedlou <balalin2003@gmail.com>
2020-05-01 12:56:36 +0300

    api attached

--- main.py
+++ main.py
@@ -36,6 +36,8 @@ def chat(cid):
     
 api.add_resource(auth.AuthResource, '/api/auth')
 api.add_resource(dial.ChatsResource, '/api/chats')
+api.add_resource(dial.UserInfoResource, '/api/users')
+api.add_resource(dial.MessageResource, '/api/messages')
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0', debug=True)
