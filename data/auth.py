18b533e4f53342fef9b33156acb9fa8a127f30d8
SuhEugene <32931701+SuhEugene@users.noreply.github.com>
2020-05-16 09:00:10 +0300

    Final (maybe)

--- data/auth.py
+++ data/auth.py
@@ -1,11 +1,15 @@
+# Smol file for authentication API
+
+
 from flask_restful import reqparse, Resource
 from flask import jsonify, session
 import sys
 from .db_funcs import UserConnector
 
+
 class AuthResource(Resource):
-    # Log in
     def get(self):
+        # Log in
         parser = reqparse.RequestParser()
         parser.add_argument('login', required=True)
         parser.add_argument('password', required=True)
@@ -19,16 +23,22 @@ class AuthResource(Resource):
                 return ({'status': "ER", 'reason': 'user not found'}, 404)
         except:
             return ({'status': "ER", 'reason': 'wrong password'}, 401)
-    # Sign up
+
     def post(self):
+        # Sign up
         parser = reqparse.RequestParser()
         parser.add_argument('login', required=True)
         parser.add_argument('email', required=True)
         parser.add_argument('password', required=True)
         args = parser.parse_args()
-        try: 
-            uid = UserConnector.new(login=args['login'], email=args['email'], password=args['password']).id
+        print(args['password'])
+        try:
+            uid = UserConnector.new(
+                login=args['login'],
+                email=args['email'],
+                password=args['password']).id
             session['logged_in'] = uid
             return ({'status': "OK", 'uid': uid}, 200)
         except BaseException as e:
-            return ({'status': "ER", 'reason': 'login or email is already taken'}, 409)
+            return ({'status': "ER",
+                     'reason': 'login or email is already taken'}, 409)
--- data/db_funcs.py
+++ data/db_funcs.py
@@ -1,3 +1,9 @@
+# This file contains classes which represent app objects
+# Those are helping API to get data
+
+
+from .connections import Base, create_session, global_init
+from .tables import *
 from hashlib import pbkdf2_hmac
 from pathlib import Path
 from datetime import datetime
@@ -9,32 +15,39 @@ if __name__ != "__main__":
     path.append(path[0] + '\\data')
 
 
-from tables import *
-from connections import Base, create_session, global_init
-
-
 global_init('db/featherDB.sqlite')
 session = create_session()
 
+pic_formats = ('gif', 'png', 'jpg', 'jpeg')
+
+
+def filetype(id):
+    # returns type of file from id (img/file)
+    f = FileConnector.from_id(id).entry.filename.rsplit(
+        '.', maxsplit=1)[-1] in pic_formats
+    return 'image' if f else 'file'
+
 
 def hashed_password(password):
-    return pbkdf2_hmac('sha512', bytes(password, 'u8'), bytes('bytes', 'u8'), 10)
-
-
-def check_password(password, user_id):
-    session = create_session()
-    return session.query(User).get(user_id).hashed_password == hashed_password(password)
+    # Hashes everything
+    return pbkdf2_hmac('sha512',
+                       bytes(password, 'u8'),
+                       bytes('bytes', 'u8'), 10)
 
 
 class BaseConnector:
-    table = None
-    table_attrs = set()
+    # Base class for app objects
+    table = None  # Table which represents object
+    table_attrs = set()  # Kwargs which are requred for .new method
+
     def __init__(self, entry):
+        # Constructs object from it's DB entry
         self.id = entry.id
         self.entry = entry
-    
+
     @classmethod
     def from_id(cls, id):
+        # Returns object from it's id in DB
         try:
             return cls(session.query(cls.table).get(id))
         except:
@@ -42,6 +55,7 @@ class BaseConnector:
 
     @classmethod
     def new(cls, table_attrs=None, **kwargs):
+        # Inserts new entry to DB and return it's object
         entry = cls.table()
         if table_attrs is None:
             table_attrs = cls.table_attrs
@@ -56,40 +70,54 @@ class BaseConnector:
 
     @classmethod
     def exists_from_id(cls, id):
+        # Checks if entry with given id exists
         return session.query(cls.table).get(id) is not None
 
 
 class MessageConnector(BaseConnector):
+    # Class of messages
     table = Message
     table_attrs = set(('text',
                        'user_id',
                        'created_date',
                        'dialog_id'))
+
     @classmethod
     def new(cls, files=[], **kwargs):
+        # In addition creates files, which message has
         msg = super().new(table_attrs=cls.table_attrs, **kwargs)
         for f in files:
             FileConnector.register_file(kwargs['dialog_id'], f, msg.id)
         return msg
 
     def to_dict(self):
+        # Returns all information about message
         files = []
         for file in self.entry.files:
+            f = file.filename.rsplit('.', maxsplit=1)[-1] in pic_formats
             d = {'filename': file.filename,
-                 'file_id': f'{file.file_access}_{file.id}'}
+                 'file_id': f'{file.file_access}_{file.id}',
+                 'type': 'image' if f else 'file'}
             files.append(d)
+        date = self.entry.created_date.isoformat(timespec='seconds')
+        login = UserConnector.from_id(self.entry.user_id).entry.login
         retval = {'id': self.id,
                   'text': self.entry.text,
                   'uid': self.entry.user_id,
-                  'datetime': self.entry.created_date.isoformat(timespec='seconds'),
-                  'files': files}
+                  'datetime': date,
+                  'files': files,
+                  'login': login}
         return retval
 
     def delete(self):
-        session.query(Message).filter(Message.id == self.id).delete()
+        # delets messge from DB
+        q = session.query(Message)
+        q.filter(Message.id == self.id).delete()
+        session.commit()
 
 
 class FileConnector(BaseConnector):
+    # Class which represents files
     table = File
     table_attrs = set(('filename',
                        'message_id',
@@ -97,12 +125,15 @@ class FileConnector(BaseConnector):
 
     @classmethod
     def register_file(cls, access, file, message_id):
-        id = cls.new(filename=file.filename, message_id=message_id, file_access=access).id
+        # Saves files from messages to hard drive
+        id = cls.new(filename=file.filename,
+                     message_id=message_id, file_access=access).id
         new_filename = f'{access}_{id}'
         file.save('user_imgs/' + new_filename)
 
 
 class UserConnector(BaseConnector):
+    # Class which represents user
     table = User
     table_attrs = set(('login',
                        'email',
@@ -110,15 +141,18 @@ class UserConnector(BaseConnector):
 
     @classmethod
     def new(cls, **kwargs):
+        # Also hashes password
         user = super().new(table_attrs=cls.table_attrs, **kwargs)
         user.entry.hashed_password = hashed_password(kwargs['password'])
         return user
 
     def check_password(self, password):
+        # Check if password is correct
         return hashed_password(password) == self.entry.hashed_password
 
     @classmethod
     def login(cls, login, password):
+        # Returns user object from from his login and password
         entry = session.query(User).filter(User.login == login).first()
         if entry is None:
             return None
@@ -130,31 +164,38 @@ class UserConnector(BaseConnector):
 
     @property
     def chats(self):
+        # Returns all chats of user
         for each in self.entry.dialogs:
             yield DialogConnector(each.dialog)
 
     @staticmethod
     def exists_from_login(login):
-        return session.query(User).filter(User.login == login).first() is not None
+        # You can also check if login is taken
+        q = session.query(User)
+        return q.filter(User.login == login).first() is not None
 
     @classmethod
     def from_login(cls, login):
+        # Returns user object from login
         entry = session.query(User).filter(User.login == login).first()
         if entry is not None:
             return cls(entry)
 
     @staticmethod
     def delete_by_id(id):
+        # Deletes user
         session.query(User).filter(User.id == id).delete()
         session.commit()
 
 
 class DialogConnector(BaseConnector):
+    # Class represents dialog
     table = Dialog
     table_attrs = set(('host_id', 'name'))
 
     @classmethod
     def new(cls, **kwargs):
+        # Also connects user to dialog
         if kwargs['name'] is None:
             kwargs['name'] = 'Chat'
         dialog = super().new(table_attrs=cls.table_attrs, **kwargs)
@@ -168,20 +209,26 @@ class DialogConnector(BaseConnector):
 
     @property
     def users(self):
+        # returns users of this dialog
         for each in self.entry.users:
             yield UserConnector(each.user)
-    
+
     @property
     def users_id(self):
+        # Returns ids of users in chat
         for each in self.entry.users:
             yield each.user_id
 
     def get_messages(self, count, offset):
-        query = session.query(Message).filter(Message.dialog_id == self.id).order_by(Message.id.desc()).offset(offset).limit(count)
+        # Returns messages in reverse order with given offset and count
+        query = session.query(Message)
+        query = query.filter(Message.dialog_id == self.id).order_by(
+            Message.id.desc()).offset(offset).limit(count)
         for each in query:
             yield MessageConnector(each)
 
     def add_users(self, users_id):
+        # Adds users to a chat
         for id in set(users_id) - set(self.users_id):
             entry = Connector()
             entry.user_id = id
@@ -190,5 +237,8 @@ class DialogConnector(BaseConnector):
         session.commit()
 
     def delete_users(self, users_id):
-        session.query(Connector).filter(Connector.dial_id == self.id, Connector.user_id.in_(users_id)).delete()
+        # Deletes user from a chat
+        q = session.query(Connector)
+        q.filter(Connector.dial_id ==
+                 self.id, Connector.user_id.in_(users_id)).delete()
         session.commit()
